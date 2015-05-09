13e1493829461dd37446d2d7c3de4414c29c3e82
seba-1511 <seba-1511@hotmail.com>
2015-05-09 01:53:01 -0700

    Ready for the first release.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the seba1511githubioApp
  */
 angular.module('seba1511githubioApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $firebase) {
+        var projectsRef = new Firebase(
+            "https://blazing-fire-2321.firebaseio.com/blogs");
+
+        $scope.blogs = $firebase(projectsRef);
+
+        $scope.blogs.$on("loaded", function() {
+            var indexes = $scope.projects.$getIndex();
+            indexes.map(function(el) {
+                var proj = $scope.projects[el];
+                proj.key = el;
+            });
+            $scope.carousel = $scope.projects.slice(0, 3);
+        });
   });
