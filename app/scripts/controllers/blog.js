62691227bcb46d321263468e835dde6598539024
seba-1511 <seba-1511@hotmail.com>
2015-05-09 00:36:51 -0700

    Added Roboto and blog reading.

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -8,10 +8,19 @@
  * Controller of the seba1511githubioApp
  */
 angular.module('seba1511githubioApp')
-  .controller('BlogCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('BlogCtrl', function($scope, $firebase, $firebaseSimpleLogin) {
+        var projectsRef = new Firebase(
+            "https://blazing-fire-2321.firebaseio.com/blogs");
+
+
+        $scope.blogs = $firebase(projectsRef);
+        $scope.auth = $firebaseSimpleLogin(projectsRef);
+
+        $scope.blogs.$on("loaded", function() {
+            var indexes = $scope.projects.$getIndex();
+            indexes.map(function(el) {
+                var proj = $scope.projects[el];
+                proj.key = el;
+            });
+        });
+    });
